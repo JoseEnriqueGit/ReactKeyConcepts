e77a666418a40ad21ac0dc96fb91d21edc5b0d24
Jose Enrique <enriquemk51@gmail.com>
2023-01-28 16:19:24 -0400

    Styles Tailwind and redering a list of products with api's platzi

--- Cap5/src/App.jsx
+++ Cap5/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 // Components
-import Card from './components/Card'
+import Card from "./components/Card";
+import ListBox from "./components/ListBox";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [products, setProducts] = useState();
 
-  return (
-    <div className="App">
-      <Card></Card>
-    </div>
-  )
+	useEffect(() => {
+		fetch("https://api.escuelajs.co/api/v1/products")
+			.then((res) => res.json())
+			.then((data) => setProducts(data));
+	}, []);
+	// console.log(products);
+
+	return (
+		<div className="flex h-2/4 w-3/4 flex-col items-center justify-center">
+			<ListBox />
+
+			<main className="m-4 flex w-full flex-wrap justify-center gap-5 overflow-auto">
+				{products &&
+					products.map((product, index) => {
+						return (
+							<Card
+								key={product.id}
+								name={product.title}
+								description={product.description}
+								src={product.images[0]}
+								price={product.price}
+							/>
+						);
+					})}
+			</main>
+		</div>
+	);
 }
 
-export default App
+export default App;
