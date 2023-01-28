4d95de50ae78438374156af08f09d15ea6abc845
Jose Enrique <enriquemk51@gmail.com>
2023-01-28 16:20:05 -0400

    Card component

--- Cap5/src/components/Card.jsx
+++ Cap5/src/components/Card.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+// Component
 
-function Card() {
-  return (
-    <h1 className='text-3xl font-bold underline'>Card</h1>
-  )
-}
+const Card = (props) => {
+	return (
+		<div className="flex h-40 w-96 flex-row rounded-md bg-lime-200 shadow-md">
+			<img className="h-40 w-36 rounded-l-lg object-cover" src={props.src} />
+			<div className="flex w-full flex-col gap-2 p-3">
+				<span className="font-bold">{props.name}</span>
+				<p className="h-9">{props.description}</p>
+				<div className="flex h-full w-full items-end justify-end">
+					<span className="font-bold">{props.price} USD</span>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Card
+export default Card;
