98d173b39c5829e7e983b686f860eff90db8717f
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-02-26 11:02:17 -0400

    TypeScriptCourso, Components

---
+++ TypeScriptCourse/src/assets/components/controls.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type ControlPanelProps = {
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const ControlPanel = ({ name, onChange }: ControlPanelProps) => {
+  return (
+    <form
+      className="flex flex-row gap-4 text-white bg-primary-10"
+      onSubmit={(event) => event.preventDefault()}
+    >
+      <div>
+        <label className="font-bold">Your Name</label>
+        <input
+          name="name"
+          className="w-full"
+          type="text"
+          value={name}
+          onChange={onChange}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default ControlPanel;
